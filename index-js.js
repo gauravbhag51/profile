0a991a5f2cd6b8d86b38b8208ff3733e7a524136
gauravbhag51 <54995043+gauravbhag51@users.noreply.github.com>
2020-06-03 12:22:46 +0530

    Update index-js.js

--- index-js.js
+++ index-js.js
@@ -1,4 +1,5 @@
-function heading_appear(){
+let playingmusic={0:""};
+function scroll_events(){
     var headings=document.querySelectorAll(".h1-animated");
     headings.forEach(heading=>{
         text=heading.getBoundingClientRect().top;
@@ -12,28 +13,46 @@ function heading_appear(){
             heading.classList.remove("h1-appear");
         }
     })
+    let images=document.querySelectorAll(".shows img");
+    images.forEach(image=>{
+        positiontop=image.getBoundingClientRect().top;
+        positionbottom=image.getBoundingClientRect().bottom;
+        var screenPosition=window.innerHeight;
+        if(positiontop>=screenPosition || positionbottom<=0)
+        {
+            if(playingmusic[0]!="")
+            {
+                playingmusic[0].pause();
+                playingmusic[0]="";
+            }
+        }
+      });
 }
-
-
 
 function playaudio(){
     let images=document.querySelectorAll(".shows img");
     var music={
         "friends":"assets/friends.mp3",
-        "stranger_things":"",
-        "breaking_bad":"",
+        "stranger_things":"assets/stranger_things.mp3",
+        "breaking_bad":"assets/breaking_bad.mp3",
         "sherlock":"assets/sherlock.mp3",
-        "himym":"",
-        "chernobyl":"",
-        "rick_and_morty":"",
+        "himym":"assets/himym.mp3",
+        "chernobyl":"assets/chernobyl.mp3",
+        "rick_and_morty":"assets/rick_and_morty.mp3",
     };
     images.forEach(image=>{
            image.addEventListener("click",()=>{
+               if(playingmusic[0]!="")
+               {
+                   playingmusic[0].pause();
+                   playingmusic[0]="";
+               }
             let name=image.classList[1];
             console.log(`${music[`${name}`]}`);
-            new Audio(`${music[`${name}`]}`).play();
+            playingmusic[0]=new Audio(`${music[`${name}`]}`);
+            playingmusic[0].play();
          });
     })
 }
-window.addEventListener('scroll',heading_appear);
+window.addEventListener('scroll',scroll_events);
 playaudio();
